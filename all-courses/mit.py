6be5737041954e17b0946230eaac7b36920de475
Chloe Qiao <qiaochloe@gmail.com>
2022-06-06 22:33:25 -0700

    Export separate json files instead of csv

--- all-courses/mit.py
+++ all-courses/mit.py
@@ -3,26 +3,6 @@ from bs4 import BeautifulSoup as bs
 import csv
 import pandas as pd
 
-#major_num = [i for i in range(1, 25) if i not in [13, 19, 23]]
-
-#for i in major_num:
-#    url = f"http://student.mit.edu/catalog/m{i}a.html"
-    
-#    request = requests.get(url).text
-#    soup = bs(request, "html.parser")
-
-#    # Count number of pages in course section
-#    contentmini = soup.find(id="contentmini")
-#    td = contentmini.find_all("td")
-#    num_pages = 0
-#    for x in td:
-#        num_pages += 1
-#    num_pages = int((num_pages - 3)/2)
-
-#    abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
-#    for x in range(0, num_pages):
-#        urls.append(f"http://student.mit.edu/catalog/m{i}{abc[x]}.html")
-
 # Get urls for each subject
 urls = []
 
@@ -35,12 +15,18 @@ for subject in notinpdf.find_all("a"):
     url = subject["href"]
     urls.append(f"http://catalog.mit.edu{url}")
     
-# Create MIT csv and set header
-mit_csv = open('mit.csv', 'w', newline='')
-writer = csv.writer(mit_csv)
+# Create csv and set header
+#courses_csv = open('courses.csv', 'w', newline='')
+#courses_writer = csv.writer(courses_csv)
 
-header = ['course_title', 'course_code', 'prereq_courses', 'same_subjects', 'course_desc']
-writer.writerow(header)
+#prereqs_csv = open('prereqs.csv', 'w', newline='')
+#prereqs_writer = csv.writer(prereqs_csv)
+
+#header = ['course_title', 'course_code', 'prereq_courses', 'same_subjects', 'course_desc']
+#writer.writerow(header)
+
+courses_df = pd.DataFrame(columns=['course_title', 'course_code', 'same_subjects', 'course_desc'])
+prereqs_df = pd.DataFrame(columns=['course', 'prereq'])
 
 for url in urls:
     request = requests.get(url)
@@ -48,11 +34,16 @@ for url in urls:
     soup = bs(html, "lxml")
     
     for element in soup.find_all(class_="courseblock"):
-        course_title = element.find(class_="courseblocktitle").text
-        course_code = course_title.split(" ")[0]
         
+        # TITLE
+        course_block_title = element.find(class_="courseblocktitle").text
+        course_title = " ".join(course_block_title.split(" ")[1:]) 
+        course_code = course_block_title.split(" ")[0] # Also captures COURSE_CODE1,\xa0COURSE_CODE12
+        
+        # PREREQS
         prereq_block = element.find(class_="courseblockprereq")
         
+        prereq_desc = ""
         try: 
             prereq_desc = prereq_block.text 
         except AttributeError:
@@ -64,16 +55,30 @@ for url in urls:
                 prereq_courses.append(prereq_course["href"].strip('/search/?P='))
         except AttributeError:
             pass
+        
+        # CLUSTERS
+        course_block_cluster = element.find(class_="courseblockcluster")
 
         same_subjects = []  
-        course_block_cluster = element.find(class_="courseblockcluster")
         if course_block_cluster: # to catch NoneType
-            for line in course_block_cluster.text.splitlines(): # to filter out Same Subject as from Subject meets with
+            for line in course_block_cluster.text.splitlines(): # to filter out "Same Subject As" from "Subject Meets With"
                 if "Same subject as" in line:
                     same_subjects = line.strip("Same subject as").split(', ')
-                        
+        
+        # DESCRIPTION
         course_desc = element.find(class_="courseblockdesc").text
         
-        row = [course_title, course_code, prereq_courses, same_subjects, course_desc]
-        writer.writerow(row)
+        course_row = [course_title, course_code,same_subjects, course_desc]
+        courses_df.loc[len(courses_df)] = course_row
         
+        for prereq in prereq_courses: 
+            prereq_row = [course_code, prereq]
+            prereqs_df.loc[len(prereqs_df)] = prereq_row
+            
+        #writer.writerow(row)
+
+#courses_df.to_csv(file_name, encoding='utf-8')
+#prereqs_df.to_csv(file_name, encoding='utf-8')
+
+courses_df.to_json('all-courses/courses.json', orient='records')
+prereqs_df.to_json('all-courses/prereqs.json', orient='records')
